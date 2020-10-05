2227d2545f785e622f5ecd2590802f9f97bbafd6
Tokyo <emil123904@gmail.com>
2020-10-05 18:36:57 +0300

    Create J figure

--- Tetris/Figures.cs
+++ Tetris/Figures.cs
@@ -146,4 +146,47 @@ namespace Tetris
                 condition++;
         }
     }
+
+    public class FigureJ : IFigure
+    {
+        int condition = 0;
+        int[][,] figure = new int[4][,]
+        {
+            new int[2, 3]
+            {
+                { 1,1,1 },
+                { 0,0,1 }
+            },
+            new int[3, 2]
+            {
+                { 1,1 },
+                { 1,0 },
+                { 1,0 }
+            },
+            new int[2, 3]
+            {
+                { 1,0,0 },
+                { 1,1,1 }
+            },
+            new int[3, 2]
+            {
+                { 0,1 },
+                { 0,1 },
+                { 1,1 }
+            }
+        };
+
+        public int[,] GetFigure()
+        {
+            return figure[condition];
+        }
+
+        public void Rotate()
+        {
+            if (condition == figure.Length)
+                condition = 0;
+            else
+                condition++;
+        }
+    }
 }
