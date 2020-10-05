bb8386e7b4b131c8bab017de7e48a0de990dbbb7
Tokyo <emil123904@gmail.com>
2020-10-05 18:26:35 +0300

    Create I figure

---
+++ Tetris/Figures.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Tetris
+{
+    interface IFigure
+    {
+        int[,] GetFigure();
+        void Rotate();
+    }
+
+    public class FigureI : IFigure
+    {
+        int condition = 0;
+        int[][,] figure = new int[2][,]
+        {
+            new int[1, 4]
+            {
+                { 1,1,1,1 }
+            },
+            new int[4, 1]
+            {
+                { 1 },
+                { 1 },
+                { 1 },
+                { 1 }
+            }
+        };
+
+        public int[,] GetFigure()
+        {
+            return figure[condition];
+        }
+
+        public void Rotate()
+        {
+            if (condition == figure.Length)
+                condition = 0;
+            else
+                condition++;
+        }
+    }
+}
---
+++ TetrisLibraryTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TetrisLibraryTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
