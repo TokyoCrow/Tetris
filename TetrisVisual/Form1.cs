273aadcee3232aefef069b7c810652cf2980e5e5
Tokyo <emil123904@gmail.com>
2020-10-17 21:53:02 +0300

    add visual logic

---
+++ TetrisVisual/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Tetr = Tetris.Tetris;
+
+namespace TetrisVisual
+{
+    public partial class Form1 : Form
+    {
+        Tetr tetris = new Tetr();
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 400;
+            timer1.Start();
+        }
+
+        public void DrawTetrominos(Graphics g)
+        {
+            for (var y = 0; y < tetris.FallingTetromino.GetTetromino().GetLength(0); y++)
+            {
+                for (var x = 0; x < tetris.FallingTetromino.GetTetromino().GetLength(1); x++)
+                {
+                    if (tetris.FallingTetromino.GetTetromino()[y, x] == 1)
+                        g.FillRectangle(Brushes.Red, new Rectangle(50 + (tetris.FallingTetrominoX + x) * 25 - 1, 50 + (tetris.FallingTetrominoY + y) * 25 + 1, 25-1,25-1));
+                }
+            }
+            
+            for (var y = 0; y < tetris.GameField.GetLength(0); y++)
+            {
+                for (var x = 0; x < tetris.GameField.GetLength(1); x++)
+                {
+                    if (tetris.GameField[y, x] == 1)
+                        g.FillRectangle(Brushes.Red, new Rectangle(50 + x * 25, 50 + y * 25, 25, 25));
+                }
+            }
+        }
+
+        public void DrawGameField(Graphics g)
+        {
+            for (var y = 0; y <= tetris.GameField.GetLength(0); y++)
+            {
+                g.DrawLine(Pens.Black, new Point(50, 50 + y * 25), new Point(50 + tetris.GameField.GetLength(1) * 25, 50 + y * 25));
+            }
+            for (var x = 0; x <= tetris.GameField.GetLength(1); x++)
+            {
+                g.DrawLine(Pens.Black, new Point(50 + x * 25, 50), new Point(50 + x * 25, 50 + tetris.GameField.GetLength(0) * 25));
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawTetrominos(e.Graphics);
+            DrawGameField(e.Graphics);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (tetris.IsGameLost)
+                timer1.Stop();
+            lblScore.Text = tetris.Score.ToString();
+            tetris.TetrominoMoveDown();
+            Invalidate();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    tetris.TetrominoRotate();
+                    Invalidate();
+                    break;
+                case Keys.Right:
+                    tetris.TetrominoMoveRight();
+                    Invalidate();
+                    break;
+                case Keys.Left:
+                    tetris.TetrominoMoveLeft();
+                    Invalidate();
+                    break;
+                case Keys.Down:
+                    tetris.TetrominoMoveDown();
+                    Invalidate();
+                    break;
+            }
+        }
+    }
+}
