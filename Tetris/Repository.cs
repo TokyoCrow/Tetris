b052654d29429078f4c448907e6a50cba7b6a5f3
Tokyo <emil123904@gmail.com>
2020-10-28 21:45:15 +0300

    Realized interface

--- Tetris/Repository.cs
+++ Tetris/Repository.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.IO;
-using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace Tetris.Core
 {
@@ -10,9 +10,9 @@ namespace Tetris.Core
         Tetris Load();
     }
 
-    class TetrisModel
+    public class TetrisModel
     {
-        public int[,] GameField { get; private set; }
+        public int[,] GameField { get; set; }
         public int Score { get;  set; }
         public bool IsGameLost { get;  set; }
         public int FallingTetrominoX { get;  set; }
@@ -40,26 +40,26 @@ namespace Tetris.Core
 
     public class JsonRepo : IRepository
     {
-        public async void Save(Tetris tetris)
+        public void Save(Tetris tetris)
         {
-            using (FileStream save = new FileStream("save.json", FileMode.OpenOrCreate))
+            using (StreamWriter sw = new StreamWriter(@"save.json"))
+            using (JsonWriter jw = new JsonTextWriter(sw))
             {
+                JsonSerializer serializer = new JsonSerializer();
                 TetrisModel tetrisModel = new TetrisModel(tetris);
-                await JsonSerializer.SerializeAsync<TetrisModel>(save, tetrisModel);
+                serializer.Serialize(jw,tetrisModel);
             }
         }
 
         public Tetris Load()
         {
-            Tetris tetris = new Tetris();
-            try
+            Tetris tetris;
+            using (StreamReader save = File.OpenText(@"save.json"))
             {
-                string tetrisJson = File.ReadAllText("save.json");
-                TetrisModel tetrisModel = JsonSerializer.Deserialize<TetrisModel>(tetrisJson);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
+                JsonSerializer serializer = new JsonSerializer();
+                TetrisModel tetrisModel = (TetrisModel)serializer.Deserialize(save, typeof(TetrisModel));
+                tetris = new Tetris(tetrisModel);
+                Console.WriteLine(tetris.Score);
             }
             return tetris;
         }
